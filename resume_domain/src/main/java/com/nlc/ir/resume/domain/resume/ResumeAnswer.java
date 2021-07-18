ab8776e1da70c52febf91ea0011a067dc73cc628
fengyuting1 <fengyuting@jd.com>
2021-07-19 00:43:13 +0800

    mybatis

--- resume_domain/src/main/java/com/nlc/ir/resume/domain/resume/ResumeAnswer.java
+++ resume_domain/src/main/java/com/nlc/ir/resume/domain/resume/ResumeAnswer.java
@@ -1,5 +1,7 @@
 package com.nlc.ir.resume.domain.resume;
 
+import lombok.Data;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -7,6 +9,7 @@ import java.util.Date;
  * resume_answer
  * @author 
  */
+@Data
 public class ResumeAnswer implements Serializable {
     private Integer id;
 
@@ -42,59 +45,4 @@ public class ResumeAnswer implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Short getNum() {
-        return num;
-    }
-
-    public void setNum(Short num) {
-        this.num = num;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Short getSubjectNum() {
-        return subjectNum;
-    }
-
-    public void setSubjectNum(Short subjectNum) {
-        this.subjectNum = subjectNum;
-    }
-
-    public Short getTagNum() {
-        return tagNum;
-    }
-
-    public void setTagNum(Short tagNum) {
-        this.tagNum = tagNum;
-    }
-
-    public Date getCreatedTime() {
-        return createdTime;
-    }
-
-    public void setCreatedTime(Date createdTime) {
-        this.createdTime = createdTime;
-    }
-
-    public Date getModifyTime() {
-        return modifyTime;
-    }
-
-    public void setModifyTime(Date modifyTime) {
-        this.modifyTime = modifyTime;
-    }
 }
